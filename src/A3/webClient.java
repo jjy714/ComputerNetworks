b3a70352c0b0d4735ba8dff5fbc43484d0ce272b
Jason Jung <jjy714@gmail.com>
2023-11-20 17:30:13 +0900

    Added image GET function

--- src/A3/webClient.java
+++ src/A3/webClient.java
@@ -3,6 +3,11 @@ package A3;
 import java.io.*;
 import java.net.*;
 import java.util.*;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class webClient {
 
@@ -40,6 +45,38 @@ public class webClient {
             conn.disconnect();
         }
         return sb.toString();
+    }
+    public void getWebContentByGetImage(String urlString, int timeout) throws IOException{
+        if (urlString == null || urlString.length() == 0) {
+            return;
+        }
+        urlString = (urlString.startsWith("http://") || urlString.startsWith("https://")) ? urlString : ("http://" + urlString).intern();
+        URL url = new URL(urlString);
+        Image image = null;
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("User-Agent", "9918920236/JUNYOUNGJUNG/WEBCLIENT/COMPUTERNETWORK");
+        conn.setRequestProperty("Accept", "image/jpeg");
+        conn.setConnectTimeout(timeout);
+
+        try {
+            if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                return;
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+            return;
+        }
+        image = ImageIO.read(url);
+        JFrame frame = new JFrame();
+        frame.setSize(500, 500);
+        JLabel label = new JLabel(new ImageIcon(image));
+        frame.add(label);
+        frame.setVisible(true);
+
+        if(conn != null){
+            conn.disconnect();
+        }
     }
 
     public String getWebContentByPost(String urlString, String data, final String charset, int timeout) throws IOException{
@@ -99,7 +136,7 @@ public class webClient {
         Scanner scanner = new Scanner(System.in);
         webClient webClient = new webClient();
 
-        System.out.print("Select the command (1: GET, 2: POST, 0: Exit): ");
+        System.out.print("Select the command (1: GET, 2: POST, 3:IMAGE, 0: Exit): ");
         command = scanner.nextInt();
         System.out.print("Enter the URL: ");
         newUrl = scanner.next();
@@ -111,6 +148,9 @@ public class webClient {
             System.out.print("Enter the data: ");
             newData = scanner.next();
             System.out.println(webClient.getWebContentByPost(newUrl, newData, "UTF-8", 10000));
+        }
+        if(command == 3){
+            webClient.getWebContentByGetImage(newUrl, 10000);
         }
         if(command == 0){
             System.exit(0);
